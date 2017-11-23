4c1e78c635cbc3e2c94591bdc481822c8d4cbe3c
neowara <32764514+neowara@users.noreply.github.com>
2017-11-23 16:31:16 +0100

    Modify and expanded JS code
    Add 3 functions for cleaner code, logout button function

--- js.js
+++ js.js
@@ -1,18 +1,73 @@
 $(document).ready(function() {
+    //page loads
+
+
+
+start();
+
+if (sessionStorage.ourUser != null){
+    // om någon är inloggad
+    console.log ("inloggad")
+    loggedin()
+} else {
+    start()
+    // Om någon inte är inloggad
+    console.log ("ej inloggad")
+
     $('#username').focus();
+    
+        $('#login').click(function() {
+    
+           event.preventDefault(); // prevent PageReLoad
+    
+           var ValidEmail = $('#username').val() === 'Test'; // User validate
+           var ValidPassword = $('#password').val() === 'password'; // Password validate
+    
+            if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
+                console.log("user ok")
+                loggedin();
+                sessionStorage.ourUser = $('#username').val();
+    
+            }        
+            else {
+                wrongpass(); // show error msg
+            }
+        });
+}
 
-    $('#submit').click(function() {
+    
+function start(){
+    $(".forgotpass").hide();
+    $(".array").hide();
+    $("#logout").hide();
+    $(".error").hide();
+    $(".valid").hide();
 
-       event.preventDefault(); // prevent PageReLoad
+}
+function loggedin(){
+    $(".loginform").hide();
+    $(".hejString").text("Welcome " + sessionStorage.ourUser);
+    /* skrivautlista(); */ 
+    $(".array").show();
+    $(".forgotpass").hide();
+    $("#logout").show();
+    $("#login").hide();
+    $(".welcomemsg").hide();
+    $(".valid").show();
+    $(".welcomemsg").hide();
+}
+function wrongpass(){
+    $(".array").hide();
+    $("#forgotpass").show();
+    $(".error").show();
+}
+    
 
-       var ValidEmail = $('#username').val() === 'test'; // User validate
-       var ValidPassword = $('#password').val() === 'password'; // Password validate
+    $('#logout').click(function() {
+        sessionStorage.clear();
+        location.reload();
 
-        if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
-            $('.valid').css('display', 'block');
-        }
-        else {
-            $('.error').css('display', 'block'); // show error msg
-        }
+
+
     });
 });
